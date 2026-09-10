19417a0b5199dafca3eddcdc9c17cc8ab9dbaa54
ahmed152515 <ahmed152515@github.com>
2026-09-10 21:27:28 +0530

    Further reduce mobile Ask height to move chat up

--- app/page.tsx
+++ app/page.tsx
@@ -871,7 +871,7 @@ function HomeContent() {
         )}
 
         {mobileTab === 'ask' && (
-          <div className="flex flex-col h-[calc(100vh-120px)]">
+          <div className="flex flex-col h-[calc(100vh-90px)]">
             <div className="flex-1 overflow-y-auto p-4 pb-0">
               {messages.length === 0 && (
                 <div className="space-y-2 mb-4">
