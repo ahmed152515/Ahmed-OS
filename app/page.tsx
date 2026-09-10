fa6b2bf72e0329c8a449b509f9363c3a695de666
ahmed152515 <ahmed152515@github.com>
2026-09-10 21:29:47 +0530

    Remove scrolling on initial load, add only when messages exist

--- app/page.tsx
+++ app/page.tsx
@@ -872,7 +872,7 @@ function HomeContent() {
 
         {mobileTab === 'ask' && (
           <div className="flex flex-col h-[calc(100vh-90px)]">
-            <div className="flex-1 overflow-y-auto p-4 pb-0">
+            <div className={`${messages.length > 0 ? 'flex-1 overflow-y-auto' : 'flex-1'} p-4 pb-0`}>
               {messages.length === 0 && (
                 <div className="space-y-2 mb-4">
                   <p className="text-xs text-gray-500 mb-2">Suggested questions:</p>
